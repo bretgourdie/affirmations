e9ae4393fbde30613209683094cb6d4c686a7fa1
Bret Gourdie <bretgourdie@gmail.com>
2016-08-24 09:05:38 -0400

    Added Metrics class

---
+++ Affirmations/Metrics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Affirmations
+{
+    public class Metrics
+    {
+        public bool IsPositive { get; protected set; }
+        public int Milliseconds { get; protected set; }
+
+        public Metrics(int milliseconds)
+        {
+            this.Milliseconds = milliseconds;
+            calculateMetrics();
+        }
+
+        protected void calculateMetrics()
+        {
+            this.IsPositive = true;
+        }
+    }
+}
